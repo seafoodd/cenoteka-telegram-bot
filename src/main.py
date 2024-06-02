1907d644a087d804c45fc4bf887fc5489f285db1
seafood <mail.seafood@proton.me>
2024-06-02 13:43:08 +0300

    edit bot commands

--- src/main.py
+++ src/main.py
@@ -2,17 +2,13 @@ from bot_init import bot
 from utils.search_utils import search_products
 from utils.message_utils import send_message
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands=['start', 'menu'])
 def handle_start(message) -> None:
     send_message(message.chat.id, "Hello! I'm a bot that will help you find products on Cenoteka.")
 
 
-@bot.message_handler(commands=['search'])
-def handle_search_command(message) -> None:
-    send_message(message.chat.id, "Enter the name of the product you want to search for.")
-
-
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=['help', 'search'])
 def handle_help_command(message) -> None:
     send_message(message.chat.id, "To search for a product, enter the name of the product you want to find.")
 
